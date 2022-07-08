0c73bde009bf703054ace12665f1f60f884589b5
shreyaswikriti <shreyaguptammm@gmail.com>
2022-07-08 09:29:04 +0530

    github action

--- main.cc
+++ main.cc
@@ -6,7 +6,7 @@ int
 main (int argc, char *argv[])
 {
   double x = 10.0;
-  std::complex<double> y{std::numeric_limits<double>::infinity (), 1.0};
+  std::complex<double> y = (std::numeric_limits<double>::infinity (), 1.0);
 
   std::complex<double> z1 = std::pow (x, y);
   std::complex<double> z2 = std::pow (std::complex<double> (x), y);
