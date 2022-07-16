22bde4c92f89be574ccd2ee91aefed76f7574183
shreyaswikriti <shreyaguptammm@gmail.com>
2022-07-17 00:04:05 +0530

    github action

--- bug3.cc
+++ bug3.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
 int main()
 {
-    typedef typename std::complex<double> Complex;
-    Complex x = Complex( 0,0 ) ;
-    Complex y = Complex(-1.904e-01, +0);
-    Complex z = Complex(-1.904e-01 ,- 9.518e-80 )  ;
-    Complex rho = sqrt(x * x + y * y + z * z);
+    
+    std::complex<double> x = std::complex<double>( 0,0 ) ;
+    std::complex<double> y = std::complex<double>(-1.904e-01, +0);
+    std::complex<double> z = std::complex<double>(-1.904e-01 ,- 9.518e-80 )  ;
+    std::complex<double> rho = sqrt(x * x + y * y + z * z);
     std::cout << "phi ="<< acos( z / rho ) << std::endl ;
 }
---
+++ bug4.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+#define cosd(x) (cos((x) * M_PI / 180))
+int main()
+{
+    for(int i=1;i<=20;i++) 
+    { std::complex<double> x( cosd(45) , 10^(-3*i) );
+    std::cout << real(x)<<"+"<<imag(x)<<"i"<<"-->"<<real(acos(x))<<"+"<<imag(acos(x))<<"i"<< std::endl ;}
+}
